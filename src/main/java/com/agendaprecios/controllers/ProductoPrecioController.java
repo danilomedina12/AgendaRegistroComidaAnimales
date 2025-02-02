3130215d056a2327d8e620d56e6dc25ba89bad6f
Danilo Medina <danilomedina666@gmail.com>
2025-02-01 21:55:48 -0300

    Se borran imports no utilizados

--- src/main/java/com/agendaprecios/controllers/ProductoPrecioController.java
+++ src/main/java/com/agendaprecios/controllers/ProductoPrecioController.java
@@ -5,7 +5,6 @@ import com.agendaprecios.models.PrecioFecha;
 import com.agendaprecios.repositories.ProductoRepository;
 import com.agendaprecios.repositories.PrecioFechaRepository;
 
-import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.util.List;
 
@@ -13,11 +12,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.time.format.DateTimeFormatter;
 
 @Controller
 public class ProductoPrecioController {
